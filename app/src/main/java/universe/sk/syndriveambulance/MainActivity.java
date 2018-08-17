31e067daeed8f0b34dd885f16c9849ca54adcc27
SrividyaKK <SrividyaKK@users.noreply.github.com>
2018-08-17 17:52:25 +0530

    Added links between Registration and Main Activities

--- app/src/main/java/universe/sk/syndriveambulance/MainActivity.java
+++ app/src/main/java/universe/sk/syndriveambulance/MainActivity.java
@@ -1,13 +1,43 @@
 package universe.sk.syndriveambulance;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etemail, etpassword;
+    private Button btn;
+    private TextView tvRegister, tvForgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("LOGIN");
+        setupUIViews();
+
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+            }
+        });
+    } // end of onCreate
+
+    private void setupUIViews() {
+        etemail = findViewById(R.id.etemail);
+        etpassword = findViewById(R.id.etpassword);
+        btn = findViewById(R.id.btn);
+        tvRegister =findViewById(R.id.tvRegister);
+        tvForgotPassword = findViewById(R.id.tvForgotPassword);
     }
-}
+
+} // end of MainActivity
