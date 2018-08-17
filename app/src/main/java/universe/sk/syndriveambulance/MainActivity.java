0f5e641ca55b62f682b32ba5e19b9daf4b6bf318
SrividyaKK <SrividyaKK@users.noreply.github.com>
2018-08-17 22:48:43 +0530

    Changed look

--- app/src/main/java/universe/sk/syndriveambulance/MainActivity.java
+++ app/src/main/java/universe/sk/syndriveambulance/MainActivity.java
@@ -1,43 +1,36 @@
 package universe.sk.syndriveambulance;
 
-import android.content.Intent;
+import android.content.Context;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText etemail, etpassword;
-    private Button btn;
-    private TextView tvRegister, tvForgotPassword;
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Before setContentView
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                                                .setDefaultFontPath("fonts/Arkip_font.ttf")
+                                                .setFontAttrId(R.attr.fontPath)
+                                                .build());
         setContentView(R.layout.activity_main);
 
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("LOGIN");
         setupUIViews();
 
-        tvRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
-            }
-        });
     } // end of onCreate
 
     private void setupUIViews() {
-        etemail = findViewById(R.id.etemail);
-        etpassword = findViewById(R.id.etpassword);
-        btn = findViewById(R.id.btn);
-        tvRegister =findViewById(R.id.tvRegister);
-        tvForgotPassword = findViewById(R.id.tvForgotPassword);
+
     }
 
 } // end of MainActivity
