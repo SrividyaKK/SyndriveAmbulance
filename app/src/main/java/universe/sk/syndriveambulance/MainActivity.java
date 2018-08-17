4b1bf8b278723eef23d0e1376ba73adc0b364915
SrividyaKK <SrividyaKK@users.noreply.github.com>
2018-08-17 23:46:01 +0530

    register activity

--- app/src/main/java/universe/sk/syndriveambulance/MainActivity.java
+++ app/src/main/java/universe/sk/syndriveambulance/MainActivity.java
@@ -1,10 +1,22 @@
 package universe.sk.syndriveambulance;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
@@ -12,6 +24,11 @@ import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 public class MainActivity extends AppCompatActivity {
 
     Button btnLogin, btnRegister;
+    RelativeLayout rootLayout;
+
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -28,13 +45,85 @@ public class MainActivity extends AppCompatActivity {
                                                 .build());
         setContentView(R.layout.activity_main);
 
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
         setupUIViews();
 
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRegisterDialog();
+            }
+        });
+
     } // end of onCreate
+
+    private void showRegisterDialog() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("REGISTER ");
+        dialog.setMessage("Please use Email to register");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View register_layout = inflater.inflate(R.layout.layout_register, null);
+
+        final MaterialEditText etName, etEmail, etPhone, etPassword, etConfirmPassword;
+        etName = register_layout.findViewById(R.id.etName);
+        etEmail = register_layout.findViewById(R.id.etEmail);
+        etPhone = register_layout.findViewById(R.id.etPhone);
+        etPassword = register_layout.findViewById(R.id.etPassword);
+        etConfirmPassword = register_layout.findViewById(R.id.etConfirmPassword);
+
+        dialog.setView(register_layout);
+
+        //Set buttons
+        dialog.setPositiveButton("Register", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+
+                // Check validation
+                if (TextUtils.isEmpty(etName.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please enter your Name", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(etEmail.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please enter your Email Address", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(etPhone.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please enter your Phone Number", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(etPassword.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please enter your Password", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(etConfirmPassword.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please confirm your Password", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (etPassword.getText().toString().length() < 8) {
+                   Snackbar.make(rootLayout, "Min 8 char required in password", Snackbar.LENGTH_SHORT).show();
+                }
+                if (!etPassword.getText().toString().equals(etConfirmPassword.getText().toString()) ) {
+                    Snackbar.make(rootLayout, "Confirm password doesn't match Password", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+    } // end of showRegisterDialog
 
     private void setupUIViews() {
         btnLogin = findViewById(R.id.btnLogin);
         btnRegister = findViewById(R.id.btnRegister);
+        rootLayout = findViewById(R.id.rootLayout);
     }
 
 } // end of MainActivity
