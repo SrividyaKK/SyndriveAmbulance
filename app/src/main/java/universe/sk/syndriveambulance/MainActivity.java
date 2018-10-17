ea082265a313a71f5e15f9fa3dca1f774bfb4296
SrividyaKK <SrividyaKK@users.noreply.github.com>
2018-10-17 22:26:37 +0530

    Firebase integration

--- app/src/main/java/universe/sk/syndriveambulance/MainActivity.java
+++ app/src/main/java/universe/sk/syndriveambulance/MainActivity.java
@@ -20,7 +20,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.rengwuxian.materialedittext.MaterialEditText;
-
 import dmax.dialog.SpotsDialog;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
@@ -30,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnLogin, btnRegister;
     RelativeLayout rootLayout;
-    String name, email, password, phone, confirmpass;
 
     FirebaseAuth auth;
     FirebaseDatabase db;
@@ -51,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
                                                 .build());
         setContentView(R.layout.activity_main);
 
+        // init firebase
         auth = FirebaseAuth.getInstance();
         db = FirebaseDatabase.getInstance();
         users = db.getReference("Users");
@@ -75,97 +74,97 @@ public class MainActivity extends AppCompatActivity {
     private void showRegisterDialog() {
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setTitle("REGISTER ");
-        // dialog.setMessage("Please use Email to register");
+//        dialog.setMessage("Please use Email to register");
 
         LayoutInflater inflater = LayoutInflater.from(this);
         View register_layout = inflater.inflate(R.layout.layout_register, null);
 
-        final MaterialEditText etName, etEmail, etPhone, etPassword, etConfirmPassword;
-        etName = register_layout.findViewById(R.id.etName);
-        etEmail = register_layout.findViewById(R.id.etEmail);
-        etPhone = register_layout.findViewById(R.id.etPhone);
-        etPassword = register_layout.findViewById(R.id.etPassword);
-        etConfirmPassword = register_layout.findViewById(R.id.etConfirmPassword);
+//        final MaterialEditText etName, etEmail, etPhone, etPassword, etConfirmPassword;
+        final MaterialEditText etName = register_layout.findViewById(R.id.etName);
+        final MaterialEditText etEmail = register_layout.findViewById(R.id.etEmail);
+        final MaterialEditText etPhone = register_layout.findViewById(R.id.etPhone);
+        final MaterialEditText etPassword = register_layout.findViewById(R.id.etPassword);
+        final MaterialEditText etConfirmPassword = register_layout.findViewById(R.id.etConfirmPassword);
 
         dialog.setView(register_layout);
-        name = etName.getText().toString().trim();
-        email = etEmail.getText().toString().trim();
-        password = etPassword.getText().toString().trim();
-        confirmpass = etConfirmPassword.getText().toString().trim();
-        phone = etPhone.getText().toString().trim();
+        final String name = etName.getText().toString();
+        final String email = etEmail.getText().toString();
+        final String password = etPassword.getText().toString();
+        final String confirmpass = etConfirmPassword.getText().toString();
+        final String phone = etPhone.getText().toString();
 
         //Set buttons
         dialog.setPositiveButton("Register", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
-                startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
+//                startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
 
                 // Check validation
-//                if (TextUtils.isEmpty(etName.getText().toString().trim())) {
-//                    Snackbar.make(rootLayout, "Please enter your Name", Snackbar.LENGTH_SHORT).show();
-//                    return;
-//                }
-//                if (TextUtils.isEmpty(etEmail.getText().toString().trim())) {
-//                    Snackbar.make(rootLayout, "Please enter your Email Address", Snackbar.LENGTH_SHORT).show();
-//                    return;
-//                }
-//                if (TextUtils.isEmpty(etPhone.getText().toString().trim())) {
-//                    Snackbar.make(rootLayout, "Please enter your Phone Number", Snackbar.LENGTH_SHORT).show();
-//                    return;
-//                }
-//                if (TextUtils.isEmpty(etPassword.getText().toString().trim())) {
-//                    Snackbar.make(rootLayout, "Please enter your Password", Snackbar.LENGTH_SHORT).show();
-//                    return;
-//                }
-//                if (TextUtils.isEmpty(etConfirmPassword.getText().toString().trim()) ) {
-//                    Snackbar.make(rootLayout, "Please confirm your Password", Snackbar.LENGTH_SHORT).show();
-//                    return;
-//                }
-//                //if (password.length() < 8) {
-//                //   Snackbar.make(rootLayout, "Min 8 char required in password", Snackbar.LENGTH_SHORT).show();
-//                //   return;
-//                //}
-//                if (!password.equals(confirmpass)) {
-//                    Snackbar.make(rootLayout, "Confirm password doesn't match Password", Snackbar.LENGTH_SHORT).show();
-//                    return;
-//                }
+                if (TextUtils.isEmpty(name)) {
+                    Snackbar.make(rootLayout, "Please enter your Name", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(email)) {
+                    Snackbar.make(rootLayout, "Please enter your Email Address", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(phone)) {
+                    Snackbar.make(rootLayout, "Please enter your Phone Number", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Snackbar.make(rootLayout, "Please enter your Password", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(confirmpass)) {
+                    Snackbar.make(rootLayout, "Please confirm your Password", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (password.length() < 8) {
+                   Snackbar.make(rootLayout, "Min 8 char required in password", Snackbar.LENGTH_SHORT).show();
+                   return;
+                }
+                if (!password.equals(confirmpass)) {
+                    Snackbar.make(rootLayout, "Confirm password doesn't match Password", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
 
                 //Register new User
-//                auth.createUserWithEmailAndPassword(email, password)
-//                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-//                            @Override
-//                            public void onSuccess(AuthResult authResult) {
-//                                //Save user to db
-//                                User user = new User();
-//                                user.setEmail(email);
-//                                user.setName(name);
-//                                user.setPassword(password);
-//                                user.setPhone(phone);
-//
-//                                // use email as key
-//                                users.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-//                                        .setValue(user)
-//                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-//                                            @Override
-//                                            public void onSuccess(Void aVoid) {
-//                                                Snackbar.make(rootLayout, "Registered successfully", Snackbar.LENGTH_SHORT).show();
-//                                            }
-//                                        })
-//                                        .addOnFailureListener(new OnFailureListener() {
-//                                            @Override
-//                                            public void onFailure(@NonNull Exception e) {
-//                                                Snackbar.make(rootLayout, "Failed " + e.getMessage(), Snackbar.LENGTH_SHORT).show();
-//                                            }
-//                                        });
-//                            }
-//                        })
-//                        .addOnFailureListener(new OnFailureListener() {
-//                            @Override
-//                            public void onFailure(@NonNull Exception e) {
-//                                Snackbar.make(rootLayout, "Failed " + e.getMessage(), Snackbar.LENGTH_SHORT).show();
-//                            }
-//                        });
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                //Save user to db
+                                User user = new User();
+                                user.setEmail(email);
+                                user.setName(name);
+                                user.setPassword(password);
+                                user.setPhone(phone);
+
+                                // use email as key
+                                users.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                        .setValue(user)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Snackbar.make(rootLayout, "Registered successfully", Snackbar.LENGTH_SHORT).show();
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Snackbar.make(rootLayout, "Failed " + e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                                            }
+                                        });
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Snackbar.make(rootLayout, "Failed " + e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                            }
+                        });
             }
         });
 
@@ -188,15 +187,15 @@ public class MainActivity extends AppCompatActivity {
         LayoutInflater inflater = LayoutInflater.from(this);
         View login_layout = inflater.inflate(R.layout.layout_login, null);
 
-        final MaterialEditText etEmail, etPassword;
-        etEmail = login_layout.findViewById(R.id.etEmail);
-        etPassword = login_layout.findViewById(R.id.etPassword);
+//        final MaterialEditText etEmail, etPassword;
+        final MaterialEditText etEmail = login_layout.findViewById(R.id.etEmail);
+        final MaterialEditText etPassword = login_layout.findViewById(R.id.etPassword);
 
         dialog.setView(login_layout);
 
-        final String email, password;
-        email = etEmail.getText().toString().trim();
-        password = etPassword.getText().toString().trim();
+//        final String email, password;
+        final String email = etEmail.getText().toString();
+        final String password = etPassword.getText().toString();
 
         //Set buttons
         dialog.setPositiveButton("Login", new DialogInterface.OnClickListener() {
@@ -210,37 +209,37 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
 
                 // Check validation
-//                if (TextUtils.isEmpty(email)) {
-//                    Snackbar.make(rootLayout, "Please enter your Email Address", Snackbar.LENGTH_SHORT).show();
-//                    return;
-//                }
-//                if (TextUtils.isEmpty(password)) {
-//                    Snackbar.make(rootLayout, "Please enter your Password", Snackbar.LENGTH_SHORT).show();
-//                    return;
-//                }
+                if (TextUtils.isEmpty(email)) {
+                    Snackbar.make(rootLayout, "Please enter your Email Address", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Snackbar.make(rootLayout, "Please enter your Password", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
 
-                //final AlertDialog waitingDialog = new SpotsDialog(MainActivity.this);
-                //waitingDialog.show();
+                final AlertDialog waitingDialog = new SpotsDialog(MainActivity.this);
+                waitingDialog.show();
 
                 // Login
-//                auth.signInWithEmailAndPassword(email, password)
-//                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-//                            @Override
-//                            public void onSuccess(AuthResult authResult) {
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
 //                                waitingDialog.dismiss();
-//                                startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
-//                                finish();
-//                            }
-//                        })
-//                        .addOnFailureListener(new OnFailureListener() {
-//                            @Override
-//                            public void onFailure(@NonNull Exception e) {
+                                startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
+                                finish();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
 //                                waitingDialog.dismiss();
-//                                Snackbar.make(rootLayout, "Failed " + e.getMessage(), Snackbar.LENGTH_SHORT).show();
-//                         //Activate SignIn Button
-//                          btnLogin.setEnabled(true);
-//                            }
-//                        });
+                                Snackbar.make(rootLayout, "Failed " + e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                         //Activate SignIn Button
+                          btnLogin.setEnabled(true);
+                            }
+                        });
             }
         });
 
